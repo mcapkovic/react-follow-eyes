4ca35bc26b48526380b76addd0ab5378317e8113
Matej Capkovic <mcapkovic@gmail.com>
2020-12-02 17:22:42 +0100

    Fix layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Face0 from './Face0';
+import Face0 from "./Face0";
 import Face1 from "./Face1";
 import Face2 from "./Face2";
 import Face3 from "./Face3";
@@ -10,19 +10,40 @@ import "./face2.css";
 import "./face3.css";
 import "./face0.css";
 
+function Button(props) {
+  const { active, ...buttonProps } = props;
+  return (
+    <button
+      {...buttonProps}
+      className={`button ${active ? "button--active" : ""}`}
+    />
+  );
+}
+
 function App() {
   const [face, setFace] = useState("face3");
 
   return (
-    <div className="App">
-      <div className="controls">
-        <p>div + js</p>
-        <button onClick={() => setFace("face0")}>face0</button>
-        <button onClick={() => setFace("face1")}>face1</button>
-        <button onClick={() => setFace("face2")}>face2</button>
-        <hr />
-        <p>svg + js</p>
-        <button onClick={() => setFace("face3")}>face3</button>
+    <div className="app">
+      <div className="tab-strip">
+        <div className="tab-strip__tabs">
+          <p>div + js </p>
+          <Button onClick={() => setFace("face0")} active={face === "face0"}>
+            buggy eyes
+          </Button>
+          <Button onClick={() => setFace("face1")} active={face === "face1"}>
+            blinking eyes
+          </Button>
+          <Button onClick={() => setFace("face2")} active={face === "face2"}>
+            centering eyes
+          </Button>
+        </div>
+        <div className="tab-strip__tabs">
+          <p>svg + js</p>
+          <Button onClick={() => setFace("face3")} active={face === "face3"}>
+            face3
+          </Button>
+        </div>
       </div>
       {face === "face0" && <Face0 />}
       {face === "face1" && <Face1 />}
